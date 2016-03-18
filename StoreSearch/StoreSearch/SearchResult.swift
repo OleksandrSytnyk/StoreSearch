7de6a0ae47d20d483b7952ebd9625a6c50cd977c
OleksandrSytnyk <amsitnik@gmail.com>
2016-03-18 12:44:50 +0200

    Extending SearchResult object, filling array of it
    with  parsed search result data and filling tableView with them

--- StoreSearch/StoreSearch/SearchResult.swift
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -9,4 +9,11 @@
 class SearchResult {
     var name = ""
     var artistName = ""
+    var artworkURL60 = ""//for a 60×60 pixel image
+    var artworkURL100 = ""//for a 100×100 pixel image
+    var storeURL = ""
+    var kind = ""
+    var currency = ""
+    var price = 0.0
+    var genre = ""
     }   
