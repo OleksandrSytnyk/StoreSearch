a5ac9d5052898c347ef96a43bbeffa3f21334638
OleksandrSytnyk <amsitnik@gmail.com>
2016-04-03 08:46:51 +0300

    Adding image downloads to put them on the landscape screen

--- StoreSearch/StoreSearch/LandscapeViewController.swift
+++ StoreSearch/StoreSearch/LandscapeViewController.swift
@@ -98,9 +98,10 @@ class LandscapeViewController: UIViewController {
         var x = marginX
         for searchResult in searchResults {
            
-            let button = UIButton(type: .System)
-            button.backgroundColor = UIColor.whiteColor()
-            button.setTitle("\(index)", forState: .Normal)
+            let button = UIButton(type: .Custom)
+            downloadImageForSearchResult(searchResult, andPlaceOnButton: button)
+            button.setBackgroundImage(UIImage(named: "LandscapeButton"),
+                forState: .Normal)
            
             button.frame = CGRect(
             x: x + paddingHorz,
@@ -144,7 +145,26 @@ class LandscapeViewController: UIViewController {
         },
         completion: nil)
     }
-
+    
+    private func downloadImageForSearchResult(searchResult: SearchResult, andPlaceOnButton button: UIButton) {
+        
+        if let url = NSURL(string: searchResult.artworkURL60) {
+        
+        let session = NSURLSession.sharedSession()
+        
+        let downloadTask = session.downloadTaskWithURL(url) {
+        [weak button] url, response, error in
+        if error == nil, let url = url, data = NSData(contentsOfURL: url),  image = UIImage(data: data) {
+        dispatch_async(dispatch_get_main_queue()) {
+        if let button = button {
+        button.setImage(image, forState: .Normal)
+                    }
+                }
+            }
+        }
+        downloadTask.resume()
+        }
+    }
 }
 
 extension LandscapeViewController: UIScrollViewDelegate {
