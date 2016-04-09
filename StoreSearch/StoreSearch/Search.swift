365ab94647030be10eab48cdb91926ad5536527f
OleksandrSytnyk <amsitnik@gmail.com>
2016-04-09 18:01:09 +0300

    Localisation for InfoPlist.strings and regional settings

--- StoreSearch/StoreSearch/Search.swift
+++ StoreSearch/StoreSearch/Search.swift
@@ -82,15 +82,19 @@ class Search {
     func urlWithSearchText(searchText: String, category: Category) -> NSURL {
         
        let entityName = category.entityName
+       let locale = NSLocale.autoupdatingCurrentLocale()//NSLocale is regional settings
+       let language = locale.localeIdentifier
+         
+        let countryCode = locale.objectForKey(NSLocaleCountryCode) as! String
         
         let escapedSearchText =
         searchText.stringByAddingPercentEncodingWithAllowedCharacters(
             NSCharacterSet.URLQueryAllowedCharacterSet())!
         
         let urlString = String(format:
-            "https://itunes.apple.com/search?term=%@&limit=200&entity=%@", escapedSearchText, entityName)
+            "https://itunes.apple.com/search?term=%@&limit=200&entity=%@&lang=%@&country=%@", escapedSearchText, entityName, language, countryCode)
         let url = NSURL(string: urlString)
-        
+        print("URL\(url!)")
         return url!
     }
     
